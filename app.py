8a1f4aafc8f65fa87c601040165640f37578f146
s-carter91 <13502@coderacademy.edu.au>
2022-10-12 14:22:00 +1000

    End of Class

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 
@@ -7,7 +8,39 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://tello_dev:passwor
 
 db = SQLAlchemy(app)
 
-print(db.__dict__)
+class Card(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    date = db.Column(db.Date)
+    status = db.Column(db.String)
+    priority = db.Column(db.String)
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print("Tables created")
+
+@app.cli.command('drop')
+def drop_db():
+    db.drop_all()
+    print("tables dropped")
+
+@app.cli.command('seed')
+def seed_db():
+    card = Card(
+        title= 'Start the project',
+        description = 'Stage 1 - Creating the database',
+        status = 'To Do',
+        priority = 'High',
+        date = date.today()
+    )
+    db.session.add(card)
+    db.session.commit()
+    print('Tables seeded')
+
 
 @app.route('/')
 def index():
